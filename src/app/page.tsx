cb38ea1ed64e04db7167a711e0ff9bcd374eac62
captainTigerYT <raunakmanna43@gmail.com>
2025-03-25 21:33:07 +0530

    basic wpm, accuracy added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='flex justify-center items-center h-[90vh]'>
+    <div className='flex flex-col justify-center items-center h-[90vh]'>
+      <div className='text-foreground/30 font-bold italic'>type</div>
       <TypingHighlighter targetText="hello" textSize="100px" />
     </div>
   );
